53b53a21f61e7007fe42c0fac7ccb13831e44c5a
PontusEdbor <pontus.edbor@gmail.com>
2020-02-14 11:18:39 +0100

    Compression almost functional

--- app/src/main/java/com/example/huffmanapp/MainActivity.kt
+++ app/src/main/java/com/example/huffmanapp/MainActivity.kt
@@ -14,19 +14,27 @@ class MainActivity : AppCompatActivity() {
         val encodeButton = findViewById<Button>(R.id.button)
         encodeButton.setOnClickListener{
             //Click of the button should compress the text in the text view and send user to a new activity showing the compressed text and compression tree
-            val testText = "What is Lorem Ipsum?\n" +
-                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
-                    "\n" +
-                    "Why do we use it?\n" +
-                    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n" +
-                    "\n" +
-                    "\n" +
-                    "Where does it come from?\n" +
-                    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32."
+            val testText = "Wild Wild West\n" +
+                    "Wild Wild West, Jim West, desperado, rough rider\n" +
+                    "No you don't want nada\n" +
+                    "None of this, gun in this, brotha runnin this,\n" +
+                    "Buffalo soldier, look it's like I told ya\n" +
+                    "Any damsel that's in distress\n" +
+                    "Be out of that dress when she meet Jim West\n" +
+                    "Rough neck so go check the law and abide\n" +
+                    "Watch your step with flex and get a hole in your side\n" +
+                    "Swallow your pride, don't let your lip react,\n" +
+                    "You don't wanna see my hand where my hip be at,\n" +
+                    "With all of this, from the start of this,\n" +
+                    "Runnin the game, James West tamin the west so remember the name\n" +
+                    "Now who ya gonna call?\n"
             var compressedText = textParser.parseText(testText)
-            var uncompressedText = textParser.decompressText(compressedText)
+            print("Uncompressed Length: ")
+            println(testText.length)
+            print("Compressed Length: ")
+            println(compressedText.size)
             println(compressedText)
-            println(uncompressedText)
+            textParser.decompressText(compressedText)
         }
     }
 }
--- app/src/main/java/com/example/huffmanapp/TextParser.kt
+++ app/src/main/java/com/example/huffmanapp/TextParser.kt
@@ -1,7 +1,6 @@
 package com.example.huffmanapp
 
 import java.util.*
-import kotlin.collections.HashMap
 
 class TextParser (){
     private var storedHashtable = Hashtable<Char,Int>()
@@ -9,7 +8,7 @@ class TextParser (){
     /*fun parseText()
       counts instances of characters in input and returns hashtable
     */
-    fun parseText(input: String): String {
+    fun parseText(input: String): List<UByte> {
         var countingTable = Hashtable<Char,Int>()
         for (character in input){
             if (countingTable.containsKey(character)){
@@ -26,18 +25,67 @@ class TextParser (){
     /*fun compressText()
       inputs a string and returns the compressed string
     */
-    fun compressText(input: String): String {
-        var compressedText = ""
+    fun compressText(input: String): List<UByte> {
+        var compressedBinary = ""
         for (character in input){
-            compressedText += huffTree.getCode(character)
+            compressedBinary += huffTree.getCode(character)
         }
-
-        return compressedText //Will actually have to compress also
+        val compressedBytes = toByteList(compressedBinary)
+        return compressedBytes //Will actually have to compress also
+    }
+    private fun toByteList(input: String): List<UByte>{
+        var resultList = mutableListOf<UByte>()
+        //Divide into chunks of 8
+        val chunkedInput = input.chunked(8)
+        for (chunk in chunkedInput){
+            resultList.add(decode(chunk).toUByte())
+        }
+        return resultList.toList()
+    }
+    private fun decode(input: String): String{
+        val reversedInput = input.reversed()
+        var iteration = 1
+        var value = 0
+        for (digit in reversedInput){
+            if (digit == '1'){
+                value += iteration
+            }
+            iteration *= 2
+        }
+        return value.toString()
     }
     /*fun decompressText()
 
     */
-    fun decompressText(input: String): String {
-        return huffTree.printWholeText(input)
+    fun decompressText(input: List<UByte>) {
+        val decompressedString = input.fold(""){acc, entry -> acc + encode(entry)}
+        for (char in decompressedString){
+            var foundChar = huffTree.takeStep(char)
+            if (foundChar != null){
+                print(foundChar)
+            }
+        }
+
+        //return huffTree.printWholeText(decompressedString)
+    }
+    fun encode(input: UByte):String{
+        var sum = input.toInt()
+        var iterator = 1
+        var value = ""
+        while (iterator*2 < sum){
+            iterator *= 2
+        }
+        while (iterator > 0){
+            if (sum < iterator){
+                iterator /= 2
+                value += "0"
+            }
+            else {
+                sum -= iterator
+                iterator /= 2
+                value += "1"
+            }
+        }
+        return value.padStart(8,'0')
     }
 }
